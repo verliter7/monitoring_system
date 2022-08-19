acded5551fa29436dbf6b9e961677f08f2c310d0
Qizheng 050711 <76-q050711@users.noreply.gitlab.topviewclub.cn>
2022-08-19 21:44:25 +0800

    impr: 完善代码逻辑

--- server/src/controller/uservitals.controller.ts
+++ server/src/controller/uservitals.controller.ts
@@ -5,13 +5,10 @@ import type { Optional } from 'sequelize/types';
 export async function createUservitals_c(ctx: Context) {
   const uservitalsInfo = ctx.request.body as Optional<any, string>;
   uservitalsInfo.ip = ctx.ip;
+  const { aid } = ctx.state;
 
-  ctx.body = {
-    code: 200,
-    success: true,
-  };
   try {
-    await createUservitals_s(uservitalsInfo);
+    await createUservitals_s(aid, uservitalsInfo);
 
     ctx.defaultResponse();
   } catch (err) {
@@ -22,8 +19,11 @@ export async function createUservitals_c(ctx: Context) {
 }
 
 export async function getUservitalsData_c(ctx: Context) {
+  const { aid } = ctx.state;
+
   try {
-    let data = await getUservitalsData_s();
+    const data = await getUservitalsData_s(aid);
+
     ctx.defaultResponse({
       code: 200,
       data: data,
